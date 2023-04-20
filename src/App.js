4ebab315f277b096d6711113c7d126d4bead2ece
kdany25 <kabadany25@gmail.com>
2023-04-20 17:01:14 +0200

    feat: added signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Login />
+			<SignUp />
 		</BrowserRouter>
 	);
 }
