d41014f6cc6b371c2d5a3b258ef8e0a118f9acc6
kdany25 <kabadany25@gmail.com>
2023-04-20 15:14:54 +0200

    feat: added login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,10 +1,48 @@
 import React from "react";
 import Navbar from "../../component/Navbar/Navbar";
+import LoginForm from "../../component/LoginForm/LoginForm";
+import Doc from "../../assets/doc.png";
 
 const Login = () => {
 	return (
 		<div>
 			<Navbar />
+			<div style={{ display: "flex" }}>
+				<div style={{ width: "60%" }}>
+					<div
+						style={{
+							marginLeft: "10%",
+							marginTop: "20%",
+							fontSize: "24px",
+						}}
+					>
+						<h1>
+							Sign In to
+							<br /> Recharge Direct
+						</h1>
+					</div>
+
+					<div
+						style={{
+							marginLeft: "10%",
+							marginTop: "5%",
+							fontSize: "16px",
+						}}
+					>
+						<h5>
+							if you don't have an account
+							<br />
+							you can register here
+						</h5>
+					</div>
+
+					<img
+						src={Doc}
+						style={{ height: "300px", width: "300px", marginLeft: '50%' }}
+					/>
+				</div>
+				<LoginForm />
+			</div>
 		</div>
 	);
 };
